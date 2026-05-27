6bc015360c2441e2c20440038bbb3765167ccbb1
Div709 <soobdiv@gmail.com>
2026-05-27 11:33:27 +0400

    Question one

---
+++ 27thMay2026/QuestionOne.py
@@ -0,0 +1,27 @@
+items_Quantity = int(input("How many items customer bought: "))
+i = 0
+items = []
+
+def calculate_total(item):
+    total = sum(item) # sum() gives sum of a list
+    return total
+
+def apply_discount(total):
+    new_total = (90/100) * total
+    return new_total
+while i < items_Quantity:
+    i = i + 1
+    item_price= int(input(f"Enter price item {i}: "))
+    items.append(item_price)
+
+    if i == items_Quantity:
+        total_price = calculate_total(items)
+        if total_price > 1000:
+            total_price_after_discount = apply_discount(total_price)
+            print(f"Total amount to pay is {total_price_after_discount}")
+        else:
+            print(f"Total amount to pay is {total_price}")
+
+
+
+

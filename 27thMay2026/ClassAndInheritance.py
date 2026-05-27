5a96ca66b2d422c9bd6f9513378bbbd1f7e3a631
Div709 <soobdiv@gmail.com>
2026-05-27 12:02:29 +0400

    Circle example

--- 27thMay2026/ClassAndInheritance.py
+++ 27thMay2026/ClassAndInheritance.py
@@ -18,3 +18,20 @@ student1 = Student("Aisha",18,"Robotics") # Here we adding info to the class
 student1.introduce() # Here we get student 1's info then using .introduce() to carry out the printing
 # student 1 object created
 # The info goes to def __init__
+import math
+
+class Circle:
+    def __init__(self,radius=1):
+        self.radius = radius
+    def setRadius(self,radius):
+        self.radius = radius
+    def getArea(self):
+        return math.pi*self.radius*self.radius
+    def get_circumference(self):
+        return 2*math.pi*self.radius
+c1 = Circle()
+print(c1.radius)
+print(c1.getArea())
+c1.setRadius(5)
+print(c1.radius)
+print(c1.getArea())

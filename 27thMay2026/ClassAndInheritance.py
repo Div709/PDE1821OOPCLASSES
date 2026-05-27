255e0b2e3f352b252a226bc68e47575ed5ceca40
Div709 <soobdiv@gmail.com>
2026-05-27 10:27:58 +0400

    Adding of comments

--- 27thMay2026/ClassAndInheritance.py
+++ 27thMay2026/ClassAndInheritance.py
@@ -1,10 +1,20 @@
 class Student:
     def __init__(self, name, age,programme):
+        # name etc are attributes that we are going to store
+        # init means initializing
+        # self shows that name is for that specific class
         self.name = name
         self.age = age
         self.programme = programme
+    # The above part is for initializing
+    # This is usually written once for a class
+
     def introduce(self):
         print(f'My name is {self.name}.')
         print(f"I study {self.programme}.")
-student1 = Student("Aisha",18,"Robotics")
-student1.introduce()
+    # introduce() is a function
+
+student1 = Student("Aisha",18,"Robotics") # Here we adding info to the class
+student1.introduce() # Here we get student 1's info then using .introduce() to carry out the printing
+# student 1 object created
+# The info goes to def __init__

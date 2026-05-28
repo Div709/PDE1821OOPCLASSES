24c2ee526b59226374f3f8eff3d7c93bb55a18ce
Div709 <soobdiv@gmail.com>
2026-05-28 19:24:27 +0400

    Making Question One Continuous

--- 27thMay2026/QuestionOne.py
+++ 27thMay2026/QuestionOne.py
@@ -9,6 +9,7 @@ def calculate_total(item):
 def apply_discount(total):
     new_total = (90/100) * total
     return new_total
+
 while i < items_Quantity:
     i = i + 1
     item_price= int(input(f"Enter price item {i}: "))
@@ -16,6 +17,7 @@ while i < items_Quantity:
 
     if i == items_Quantity:
         total_price = calculate_total(items)
+        i = 0
         if total_price > 1000:
             total_price_after_discount = apply_discount(total_price)
             print(f"Total amount to pay is {total_price_after_discount}")

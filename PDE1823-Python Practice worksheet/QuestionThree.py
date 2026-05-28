b4b78b08c0d97d45a2e94cc084ab395b7cf7712c
Div709 <soobdiv@gmail.com>
2026-05-28 22:14:58 +0400

    Question 3 completed

---
+++ PDE1823-Python Practice worksheet/QuestionThree.py
@@ -0,0 +1,14 @@
+# Question 3
+shopping_list= [ ]
+def store_in_list(name):
+    shopping_list.append(name)
+i = 0
+while i < 5:
+    i+=1
+    user_input = input("Enter an item: ")
+    store_in_list(user_input)
+
+print(f"Your shopping list is:")
+for item in shopping_list:
+    print(item)
+

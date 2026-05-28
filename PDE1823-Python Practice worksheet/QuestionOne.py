295909bd7a9bcd0fd187a39a7a8638b715e2e7d8
Div709 <soobdiv@gmail.com>
2026-05-28 21:59:36 +0400

    Question 1 completed

---
+++ PDE1823-Python Practice worksheet/QuestionOne.py
@@ -0,0 +1,7 @@
+# Question 1
+def hello(name,age):
+    print(f'Hello {name}, you are {age} years old')
+    print('Welcome to Python programming!')
+name_input = input('Enter your name: ')
+age_input = input('Enter your age: ')
+hello(name_input,age_input)
